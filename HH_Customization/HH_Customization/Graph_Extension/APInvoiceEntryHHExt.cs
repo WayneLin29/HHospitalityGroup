dae0765e5bcbab0591732c9acb0f494cd566eb20
Alton.lee <woot2130@gmail.com>
2023-01-13 15:53:19 +0800

    隨時link檔案

---
+++ HH_Customization/HH_Customization/Graph_Extension/APInvoiceEntryHHExt.cs
@@ -0,0 +1,53 @@
+﻿using PX.Data;
+using PX.Objects.PO;
+using System;
+
+namespace PX.Objects.AP
+{
+    public class APInvoiceEntryHHExt : PXGraphExtension<APInvoiceEntry>
+    {
+        #region View
+        public PXSelect<NoteDoc,
+                Where<NoteDoc.noteID, Equal<Current<APInvoice.noteID>>>> FileLinks;
+        #endregion
+
+        #region Override
+        public delegate void PersistDelegate();
+        [PXOverride]
+        public virtual void Persist(PersistDelegate baseMethod)
+        {
+            using (PXTransactionScope ts = new PXTransactionScope())
+            {
+                //baseMethod();
+                LinkFileByPO();
+                baseMethod();
+                ts.Complete();
+            }
+        }
+        #endregion
+
+        #region Method
+        protected virtual void LinkFileByPO()
+        {
+            APInvoice invoice = Base.Document.Current;
+            //Clear All Link by header
+            foreach (NoteDoc doc in FileLinks.Select()) {
+                FileLinks.Delete(doc);
+            }
+
+            foreach (APTran tran in Base.Transactions.Select())
+            {
+                //Copy Header
+                POOrder order = POOrder.PK.Find(Base, tran.POOrderType, tran.PONbr);
+                PXCache<POOrder> poCache = Base.Caches<POOrder>();
+                PXNoteAttribute.CopyNoteAndFiles(poCache, order, Base.Document.Cache, invoice, false, true);
+                //Copy Detail
+                POLine line = POLine.PK.Find(Base, tran.POOrderType, tran.PONbr, tran.POLineNbr);
+                PXCache<POLine> lineCache = Base.Caches<POLine>();
+                PXNoteAttribute.CopyNoteAndFiles(lineCache, line, Base.Transactions.Cache, tran, false, true);
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ HH_Customization/HH_Customization/Graph_Extension/APPaymentEntryHHExt.cs
@@ -0,0 +1,49 @@
+﻿using PX.Data;
+using PX.Objects.PO;
+using System;
+
+namespace PX.Objects.AP
+{
+    public class APPaymentEntryHHExt : PXGraphExtension<APPaymentEntry>
+    {
+        #region View
+        public PXSelect<NoteDoc,
+                Where<NoteDoc.noteID, Equal<Current<APInvoice.noteID>>>> FileLinks;
+        #endregion
+
+        #region Override
+        public delegate void PersistDelegate();
+        [PXOverride]
+        public virtual void Persist(PersistDelegate baseMethod)
+        {
+            using (PXTransactionScope ts = new PXTransactionScope())
+            {
+                //baseMethod();
+                LinkFileByAdjdAP();
+                baseMethod();
+                ts.Complete();
+            }
+        }
+        #endregion
+
+        #region Method
+        protected virtual void LinkFileByAdjdAP()
+        {
+            APPayment payment = Base.Document.Current;
+            //Clear All Link by header
+            foreach (NoteDoc doc in FileLinks.Select()) {
+                FileLinks.Delete(doc);
+            }
+
+            foreach (APAdjust tran in Base.Adjustments.Select())
+            {
+                //Copy Header
+                APInvoice invoice = APInvoice.PK.Find(Base, tran.AdjdDocType, tran.AdjdRefNbr);
+                PXCache<APInvoice> invCache = Base.Caches<APInvoice>();
+                PXNoteAttribute.CopyNoteAndFiles(invCache, invoice, Base.Document.Cache, payment, false, true);
+            }
+        }
+        #endregion
+
+    }
+}
