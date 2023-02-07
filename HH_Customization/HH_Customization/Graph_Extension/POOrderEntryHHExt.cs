2361f1679b73f4446578908df7e2f2e6b1b33847
Alton.lee <woot2130@gmail.com>
2023-02-07 18:00:21 +0800

    改為每次存檔檢查，且提醒是否要重新關聯

--- HH_Customization/HH_Customization/Graph_Extension/POOrderEntryHHExt.cs
+++ HH_Customization/HH_Customization/Graph_Extension/POOrderEntryHHExt.cs
@@ -1,14 +1,34 @@
 ﻿using PX.Data;
+using PX.Objects.GL;
 
 namespace PX.Objects.PO
 {
-    public class POOrderEntryHHExt:PXGraphExtension<POOrderEntry>
+    public class POOrderEntryHHExt : PXGraphExtension<POOrderEntry>
     {
+        #region Message
+        public const string WANT_TO_LINK_BRANCH = " Inconsistent to Line Branch and Document Branch. Click Yes if you want to update document branch to {0}";
+        #endregion
+
         #region Event
-        protected virtual void _(Events.RowPersisting<POOrder> e, PXRowPersisting baseMethod) {
-            baseMethod?.Invoke(e.Cache,e.Args);
+        protected virtual void _(Events.RowPersisting<POOrder> e, PXRowPersisting baseMethod)
+        {
+            baseMethod?.Invoke(e.Cache, e.Args);
             if (e.Row == null) return;
-            LinkBranch();
+            if (Base.Document.Cache.GetStatus(Base.Document.Current) == PXEntryStatus.Inserted)
+            {
+                LinkBranch();
+            }
+            //2023-02-03 每次存檔都檢核且提醒是否更新
+            POOrder order = Base.Document.Current;
+            POLine line = Base.Transactions.Select();
+            if (line.BranchID != null && order.BranchID != line.BranchID)
+            {
+                Branch branch = Branch.PK.Find(Base, line.BranchID);
+                if (Base.Document.Ask(string.Format(WANT_TO_LINK_BRANCH, branch.BranchCD), MessageButtons.YesNo) == WebDialogResult.Yes)
+                {
+                    LinkBranch();
+                }
+            }
         }
         #endregion
 
@@ -16,16 +36,13 @@ namespace PX.Objects.PO
         protected virtual void LinkBranch()
         {
             POOrder order = Base.Document.Current;
-            if (Base.Document.Cache.GetStatus(order) == PXEntryStatus.Inserted)
+            POLine line = Base.Transactions.Select();
+            if (line?.BranchID != null)
             {
-                POLine line = Base.Transactions.Select();
-                if (line?.BranchID != null)
-                {
-                    Base.Document.Cache.SetValueExt<POOrder.branchID>(order, line.BranchID);
-                    Base.Document.UpdateCurrent();
-                }
+                Base.Document.Cache.SetValueExt<POOrder.branchID>(order, line.BranchID);
+                Base.Document.UpdateCurrent();
             }
         }
-        #endregion
     }
+    #endregion
 }
