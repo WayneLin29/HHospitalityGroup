2d1290907e09f81743f2c45da07b67e7ad4c6316
Alton.lee <woot2130@gmail.com>
2023-02-08 15:17:07 +0800

    add dialog : Update Billing Base
    針對Type = Normal 且沒有產生AP Bill 或 PO Receipt 可修改

--- HH_Customization/HH_Customization/Graph_Extension/POOrderEntryHHExt.cs
+++ HH_Customization/HH_Customization/Graph_Extension/POOrderEntryHHExt.cs
@@ -1,15 +1,53 @@
 ﻿using PX.Data;
+using PX.Objects.AP;
 using PX.Objects.GL;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PX.Objects.PO
 {
     public class POOrderEntryHHExt : PXGraphExtension<POOrderEntry>
     {
         #region Message
-        public const string WANT_TO_LINK_BRANCH = " Inconsistent to Line Branch and Document Branch. Click Yes if you want to update document branch to {0}";
+        public const string WANT_TO_LINK_BRANCH = "Inconsistent to Line Branch and Document Branch. Click Yes if you want to update document branch to {0}";
+        #endregion
+
+        #region View
+        public PXFilter<EditPOAccrualType> EditPOAccrualTypeForm;
+        #endregion
+
+        #region Action
+        public PXAction<POOrder> updateBillingBase;
+
+        [PXButton(CommitChanges = true), PXUIField(DisplayName = "Update Billing Base", MapEnableRights = PXCacheRights.Select, MapViewRights = PXCacheRights.Select)]
+        public IEnumerable UpdateBillingBase(PXAdapter adapter)
+        {
+            if (EditPOAccrualTypeForm.AskExt() == WebDialogResult.OK)
+            {
+                foreach (POLine line in Base.Transactions.Select())
+                {
+                    line.POAccrualType = EditPOAccrualTypeForm.Current.POAccrualType;
+                    Base.Transactions.Update(line);
+                }
+            }
+            EditPOAccrualTypeForm.Cache.Clear();
+            return adapter.Get();
+        }
         #endregion
 
         #region Event
+        protected virtual void _(Events.RowSelected<POOrder> e, PXRowSelected baseMethod)
+        {
+            baseMethod?.Invoke(e.Cache, e.Args);
+            if (e.Row == null) return;
+            bool isLinkAP = GetLinkAPTran(e.Row.OrderNbr, e.Row.OrderType).Count > 0;
+            bool isLinkReceipt = GetLinkPOReceiptLine(e.Row.OrderNbr, e.Row.OrderType).Count > 0;
+            bool isNormal = POOrderType.IsNormalType(e.Row.OrderType);
+            bool hasDetail = Base.Transactions.Select().Count > 0;
+            updateBillingBase.SetEnabled(isNormal && !isLinkAP && !isLinkReceipt && hasDetail);
+        }
+
         protected virtual void _(Events.RowPersisting<POOrder> e, PXRowPersisting baseMethod)
         {
             baseMethod?.Invoke(e.Cache, e.Args);
@@ -43,6 +81,40 @@ namespace PX.Objects.PO
                 Base.Document.UpdateCurrent();
             }
         }
+        #endregion
+
+        #region BQL
+        protected virtual List<APTran> GetLinkAPTran(string orderNbr, string orderType)
+        {
+            return PXSelect<APTran,
+                Where<APTran.pONbr, Equal<Required<APTran.pONbr>>,
+                And<APTran.pOOrderType, Equal<Required<APTran.pOOrderType>>>>>
+                .Select(Base, orderNbr, orderType).RowCast<APTran>().ToList();
+        }
+
+        protected virtual List<POReceiptLine> GetLinkPOReceiptLine(string orderNbr, string orderType)
+        {
+            return PXSelect<POReceiptLine,
+                Where<POReceiptLine.pONbr, Equal<Required<POReceiptLine.pONbr>>,
+                And<POReceiptLine.pOType, Equal<Required<POReceiptLine.pOType>>>>>
+                .Select(Base, orderNbr, orderType).RowCast<POReceiptLine>().ToList();
+        }
+        #endregion
+
+        #region Table
+        [PXHidden]
+        public class EditPOAccrualType : IBqlTable
+        {
+            #region POAccrualType
+            [PXString(1, IsFixed = true)]
+            [POAccrualType.List]
+            [PXUIField(DisplayName = "Billing Based On")]
+            [PXUnboundDefault(typeof(POAccrualType.order))]
+            public virtual string POAccrualType { get; set; }
+            public abstract class pOAccrualType : PX.Data.BQL.BqlString.Field<pOAccrualType> { }
+            #endregion
+        }
+        #endregion
     }
-    #endregion
+
 }
