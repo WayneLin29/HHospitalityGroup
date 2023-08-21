c2dfcb1b5f1139cdaa121b37069ca69bdbdc551a
Alton.lee <woot2130@gmail.com>
2023-08-21 15:48:05 +0800

    添加首次存檔Branch連動

---
+++ HH_Customization/HH_Customization/Graph_Extension/ARInvoiceEntryHHExt.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+using PX.Objects.GL;
+
+namespace PX.Objects.AR
+{
+    public class ARInvoiceEntryHHExt : PXGraphExtension<ARInvoiceEntry>
+    {
+        #region Override
+        #region Release
+        public delegate IEnumerable ReleaseDelegate(PXAdapter adapter);
+        [PXOverride]
+        public IEnumerable Release(PXAdapter adapter, ReleaseDelegate baseMethod)
+        {
+            if (ValidateBranch() == WebDialogResult.OK)
+                return baseMethod(adapter);
+            else
+                return adapter.Get();
+        }
+        #endregion
+        #endregion
+
+        #region Event
+        protected virtual void _(Events.RowPersisting<ARInvoice> e)
+        {
+            LinkBranch();
+        }
+        #endregion
+
+        #region Method
+        /// <summary>
+        /// ARInvoice新增時自動綁定明細第一筆BranchID
+        /// + ARTaxTran 也要更新BranchID
+        /// </summary>
+        /// <returns></returns>
+        protected virtual void LinkBranch()
+        {
+            ARInvoice invoice = Base.Document.Current;
+            if (Base.Document.Cache.GetStatus(invoice) == PXEntryStatus.Inserted)
+            {
+                ARTran tran = Base.Transactions.Select();
+                if (tran?.BranchID != null)
+                {
+                    Base.Document.Cache.SetValueExt<ARInvoice.branchID>(invoice, tran.BranchID);
+                    Base.Document.UpdateCurrent();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 檢核明細BranchID 是否與表頭一致，不一致則跳確認
+        /// </summary>
+        /// <returns></returns>
+        protected virtual WebDialogResult ValidateBranch()
+        {
+            ARInvoice invoice = Base.Document.Current;
+            foreach (ARTran tran in Base.Transactions.Select())
+            {
+                if (invoice.BranchID != tran.BranchID)
+                {
+                    Branch hb = Branch.PK.Find(Base, invoice.BranchID);
+                    Branch db = Branch.PK.Find(Base, tran.BranchID);
+                    return Base.Document.Ask(
+                        $"Document Branch - [{hb?.BranchCD?.Trim()}] differs from[{db?.BranchCD?.Trim()}].This will result in inter - company posting.Are you sure you want to proceed ?"
+                        , MessageButtons.OKCancel);
+                }
+            }
+            return WebDialogResult.OK;
+        }
+        #endregion
+    }
+}
---
+++ HH_Customization/HH_Customization/Graph_Extension/ARPaymentEntryHHExt.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+using PX.Objects.CA;
+using PX.Objects.GL;
+
+namespace PX.Objects.AR
+{
+    public class ARPaymentEntryHHExt : PXGraphExtension<ARPaymentEntry>
+    {
+        #region Override
+        #region Release
+        public delegate IEnumerable ReleaseDelegate(PXAdapter adapter);
+        [PXOverride]
+        public IEnumerable Release(PXAdapter adapter, ReleaseDelegate baseMethod)
+        {
+            if (ValidateBranch() == WebDialogResult.OK)
+                return baseMethod(adapter);
+            else
+                return adapter.Get();
+        }
+        #endregion
+        #endregion
+
+        #region Event
+        protected virtual void _(Events.RowPersisting<ARPayment> e)
+        {
+            LinkBranch();
+        }
+        #endregion
+
+        #region Method
+        /// <summary>
+        /// ARPayment新增時自動綁定CashAccount.BranchID
+        /// </summary>
+        /// <returns></returns>
+        protected virtual void LinkBranch()
+        {
+            var doc = Base.Document.Current;
+            if (Base.Document.Cache.GetStatus(doc) == PXEntryStatus.Inserted)
+            {
+                var ca = CashAccount.PK.Find(Base, doc.CashAccountID);
+                Base.Document.Cache.SetValueExt<ARInvoice.branchID>(doc, ca?.BranchID);
+                Base.Document.UpdateCurrent();
+            }
+        }
+
+        /// <summary>
+        /// 檢核明細BranchID 是否與表頭一致，不一致則跳確認
+        /// </summary>
+        /// <returns></returns>
+        protected virtual WebDialogResult ValidateBranch()
+        {
+            var doc = Base.Document.Current;
+            var ca = CashAccount.PK.Find(Base, doc.CashAccountID);
+            Branch hb = Branch.PK.Find(Base, doc.BranchID);
+            Branch db = Branch.PK.Find(Base, ca?.BranchID);
+            if (doc.BranchID != ca?.BranchID)
+            {
+                return Base.Document.Ask(
+                            $"Cash Account {db?.BranchCD} is differenct from Finance {hb?.BranchCD}. This will lead to inter-branch posting, please confirm whether you want to continue."
+                            , MessageButtons.OKCancel);
+            }
+            return WebDialogResult.OK;
+        }
+        #endregion
+    }
+}
