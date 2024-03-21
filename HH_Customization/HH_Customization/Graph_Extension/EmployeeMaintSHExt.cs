e8cec9bbc08e58a1ca193235967323f70ae485bc
Alton.lee <woot2130@gmail.com>
2024-03-21 11:42:39 +0800

    Employee Add Accountability tab

---
+++ HH_Customization/HH_Customization/Graph_Extension/EmployeeMaintSHExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HH_Customization.DAC;
+using PX.Data;
+using PX.Data.BQL.Fluent;
+
+
+namespace PX.Objects.EP
+{
+    public class EmployeeMaintSHExt : PXGraphExtension<EmployeeMaint>
+    {
+        public SelectFrom<LUMAccountability>
+            .Where<LUMAccountability.bAccountID.IsEqual<EPEmployee.bAccountID.FromCurrent>>
+            .View Accountabilitys;
+    }
+}
