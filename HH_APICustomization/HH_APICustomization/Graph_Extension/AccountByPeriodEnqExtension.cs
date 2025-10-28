4df021d5e41dfe2e8aae116467fc6b110994c895
LUMINA-WAYNELIN\JO3JO <wayne.lin@lumina-tech.com>
2025-10-28 11:17:28 +0800

    1.Adjust GLTranR 欄位計算方式

--- HH_APICustomization/HH_APICustomization/Graph_Extension/AccountByPeriodEnqExtension.cs
+++ HH_APICustomization/HH_APICustomization/Graph_Extension/AccountByPeriodEnqExtension.cs
@@ -1,25 +1,18 @@
 ﻿using PX.Data;
+using PX.Objects.CS;
 using PX.Objects.GL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static PX.Objects.FA.FAAccrualTran;
 
 namespace PX.Objects.GL
 {
     public class AccountByPeriodEnqExtension : PXGraphExtension<AccountByPeriodEnq>
     {
-        public virtual void _(Events.RowSelected<GLTranR> e, PXRowSelected baseMethod)
-        {
-            baseMethod?.Invoke(e.Cache, e.Args);
-            var row = e.Row;
-            var netAmount = (row?.DebitAmt ?? 0) - (row?.CreditAmt ?? 0);
-            var absAmount = Math.Abs(netAmount);
-
-            e.Cache.SetValueExt<GLTranRExt.usrNetAmount>(e.Row, netAmount);
-            e.Cache.SetValueExt<GLTranRExt.usrAbsAmount>(e.Row, absAmount);
-        }
+        
     }
 
     public class GLTranRExt : PXCacheExtension<GLTranR>
@@ -27,12 +20,21 @@ namespace PX.Objects.GL
         #region UsrNetAmount
         [PXDecimal]
         [PXUIField(DisplayName = "Net Amount", Enabled = false)]
+        [PXFormula(typeof(Sub<GLTranR.debitAmt, GLTranR.creditAmt>))]
         public virtual decimal? UsrNetAmount { get; set; }
         public abstract class usrNetAmount : PX.Data.BQL.BqlDecimal.Field<usrNetAmount> { }
         #endregion
 
         #region UsrAbsAmount
         [PXDecimal]
+        [PXFormula(
+                typeof(Switch<
+                    Case<Where<Sub<GLTranR.debitAmt, GLTranR.creditAmt>, Less<decimal0>>,
+                        Minus<Sub<GLTranR.debitAmt, GLTranR.creditAmt>>
+                    >,
+                    Sub<GLTranR.debitAmt, GLTranR.creditAmt>
+                >)
+            )]
         [PXUIField(DisplayName = "Abs Amount", Enabled = false)]
         public virtual decimal? UsrAbsAmount { get; set; }
         public abstract class usrAbsAmount : PX.Data.BQL.BqlDecimal.Field<usrAbsAmount> { }
